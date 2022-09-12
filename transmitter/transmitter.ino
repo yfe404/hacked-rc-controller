517fd74fa08a40f151d6eefa98c3dc6ba0d46ddc
yafeunteun <yfe.github@protonmail.com>
2022-09-12 16:39:26 +0200

    feat: I2C setup + send data

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,10 +1,17 @@
+#include <Wire.h>
+
+#define I2C_DEV_ADDR 0x55
+#define SDA_PIN 21
+#define SCL_PIN 22
+
 // Define the inputs
 #define GAZ 25  
 #define YAW 26 
 #define PITCH 34 
 #define ROLL 35
 
-int gaz, yaw, pitch, roll;
+
+byte gaz, yaw, pitch, roll;
 
 struct Data_Package {
   byte gaz;
@@ -15,9 +22,21 @@ struct Data_Package {
 
 Data_Package data; //Create a variable with the above structure
 
+char buffer[20];
+
+void onRequest(){
+ sprintf(buffer, "%03d,%03d,%03d,%03d", data.gaz, data.yaw, data.pitch, data.roll);
+ Wire.print(buffer);
+}
+
 
 void setup() {
    Serial.begin(115200);
+
+   Wire.onRequest(onRequest);
+   Wire.setClock(100000);
+   Wire.setPins(SDA_PIN, SCL_PIN);
+   Wire.begin((uint8_t)I2C_DEV_ADDR);
 
    // Set initial default values
   data.gaz = 127;
@@ -32,11 +51,5 @@ void loop() {
   data.yaw = map(analogRead(YAW), 790, 3115, 0, 255);
   data.pitch = map(analogRead(PITCH), 715, 3100, 0, 255);
   data.roll = map(analogRead(ROLL), 810, 3150, 0, 255);
-
   
-  Serial.print(data.roll);
-  Serial.print(" ");
-  Serial.println(analogRead(ROLL));
-
-  delay(200);
 }
